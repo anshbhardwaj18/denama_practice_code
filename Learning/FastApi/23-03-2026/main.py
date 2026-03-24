2c4ec9c65981ff5ef24c965faf5eabca739cd0e8
anshbhardwaj18 <anshb457@gmail.com>
2026-03-24 18:00:50 +0530

    Edit some fastapi files

--- Learning/FastApi/23-03-2026/main.py
+++ Learning/FastApi/23-03-2026/main.py
@@ -1,12 +1,33 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-items = {
-    "apple" : "A juice fruit",
-    "banana" : "A yellow color fruit"
-}
+# items = {
+#     "apple" : "A juice fruit",
+#     "banana" : "A yellow color fruit"
+# }
 
-@app.get("/items/{items_id}")
-async def get_data(items_id: str):
-    return items[items_id]
+#USING HTTPException
+# @app.get("/items/{items_id}")
+# async def get_data(items_id: str):
+#     if items_id not in items:
+#         raise HTTPException(status_code=404, detail="Page not found")
+#     return items[items_id]
+
+# Custom Header
+# @app.get("/items{items_id}")
+# async def get_data(items_id : str):
+#     if items_id not in items:
+#         raise HTTPException(
+#         status_code=404, 
+#         detail="Page not found",
+#         headers= {"x-error-type" : "itemmissing"}
+#         )
+#     return items[items_id]
+
+
+# ********************************* CUSTOM EXCEPTION ************************************
+class fruitException(Exception):
+    def __init__(self, fruit_name : str):
+        self.fruit_name = fruit_name
