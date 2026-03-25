01b3412d11c50a0438c6b943020d0bd0c035b3cb
anshbhardwaj18 <anshb457@gmail.com>
2026-03-25 15:51:21 +0530

    add new work in 23-03-2026 file

--- Learning/FastApi/23-03-2026/main.py
+++ Learning/FastApi/23-03-2026/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.exceptions import RequestValidationError
 app = FastAPI()
+
+fruits = {
+    "apple" : "A fruit juice",
+    "banana" : "A yellow delight"
+}
 
 # items = {
 #     "apple" : "A juice fruit",
@@ -28,6 +33,29 @@ app = FastAPI()
 
 
 # ********************************* CUSTOM EXCEPTION ************************************
-class fruitException(Exception):
-    def __init__(self, fruit_name : str):
-        self.fruit_name = fruit_name
+# class fruitException(Exception):
+#     def __init__(self, fruit_name : str):
+#         self.fruit_name = fruit_name
+
+# @app.exception_handler(fruitException)
+# async def fruit_exception_handler(request : Request, exc : fruitException):
+#     return JSONResponse(
+#         status_code = 418,
+#         content= {"message" : f"{exc.fruit_name} is not valid"}
+#     )
+
+# @app.get("/fruits/{fruit_name}")
+# async def get_fruit(fruit_name : str):
+#     if fruit_name not in fruits :
+#         raise fruitException(fruit_name= fruit_name)
+#     return fruits[fruit_name]
+
+
+# ********************************* OVERRIDE DEFAULT EXCEPTION HANDLERS ****************
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc:RequestValidationError):
+    return PlainTextResponse(str(exc), status_code=404)
+
+@app.get("/items/{items_id}")
+async def read_items(items_id : int):
+    return items_id
